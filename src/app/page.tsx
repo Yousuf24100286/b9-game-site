39c2e3e9614accd1fbb3cac76e58ba9659c40efc
Yousuf Khan <24100286@lums.edu.pk>
2024-09-11 15:11:22 +0500

    add header & footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from 'next/link';
+import React from 'react';
 import { H2, H3, H5, P } from '@/components/Typography'
 import { Button } from '@/components/ui/button';
 import { 
@@ -14,125 +15,134 @@ import {
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center gap-8 max-w-screen-sm text-center mx-auto my-20">
-      <H2>B9 Game Download (Earning APP Pakistan) For Android 2024</H2>
-      <Link href="https://m.b9game3.com/refer/MDM0NTM0OTQ3NzM=">
-        <Button size={'lg'} className='min-w-60 bg-green-800 text-lg font-bold'>
-          Download
-        </Button>
-      </Link>
-      <div>
-        <Table className="w-72 text-left border-none">
-          <TableBody>
-            <TableRow>
-              <TableCell>
-                Size
-              </TableCell>
-              <TableCell>
-                23.78 MB
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>
-                Version
-              </TableCell>
-              <TableCell>
-                v1.0.22
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>
-                Requirements
-              </TableCell>
-              <TableCell>
-                Android 6+
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>
-                Category
-              </TableCell>
-              <TableCell>
-                Card
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>
-                Download
-              </TableCell>
-              <TableCell>
-                Free of cost
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-        
-      </div>
-      <Image src="/banner-image-hd.png" alt="Picture of the author" className="max-w-screen-lg  w-screen" width={500} height={500} />
-      <P>The B9 game is the Latest earing Application that you can play on your phone, computer, or tablet. In this game, you get a bingo card with numbers on it, and the numbers are called out, and you can mark them off. In B9 players get a virtual bingo card filled with numbers arranged in a grid. When numbers are drawn randomly players mark off corresponding numbers on their cards. B9 gaming is free of cost for Android and the account opening bonus is 15 rupees. The main protection is a referral bonus for players. Everyone can use their account ID as an invitation code. It provides unlimited earnings for players in Pakistan.</P>
-      <P>B9 includes features like power-up players and a customizable theme and also makes it an engaging experience for players of all ages. By playing the B9 game you can earn real money while playing your favorite game. This game is made to challenge your brain while keeping you entertained for hours. A small indie team developed the B9 game. It was first released in 2024 and became unique because of its simplicity and strategy.  It is available for only Android platforms. You can download it from the Google Play or app store.</P>
-      <div className="w-full">
-        <H3 className="">B9 Bonus</H3>
-        <P>it&apos;s a cooperative game to earn a bonus in rupees, for the game bonus play more and more. There is no limit to playing and bonus collection. In many ways, you can earn a bonus by using tricks and tips.</P>
-      </div>
-      <div className="w-full">
-        <H3 className="">B9 Tips And Tricks</H3>
-        <P>trips and tricks are used by players to get more cash on gameplay. Use IQ to detect game playouts and easily win. In the other hand you can use your game ID account to get more cash on friends&apos; inventions and </P>
-      </div>
-      <div className="w-full">
-        <H3 className="">B9 Game Features</H3>
-        <ol className="">
-          <li>
-            <H5>3x3 Grid</H5>
-            <P>A small bingo card with 9 numbers in a 3x3 layout.</P>
-          </li>
-          <li>
-            <H5>Fast Gameplay</H5>
-            <P>Fastly round due to reduced number of numbers.</P>
-          </li>
-          <li>
-            <H5>Winning patterns</H5>
-            <P>Players can win by making specific patterns.</P>
-          </li>
-          <li>
-            <H5>Online features</H5>
-            <P>Includes many chats, options, and customization.</P>
-          </li>
-          <li>
-            <H5>Auto Daubing</H5>
-            <P>There are options to automatically mark called numbers.</P>
-          </li>
-          <li>
-            <H5>Number pool</H5>
-            <P>Few numbers are called compared to traditional bingo.</P>
-          </li>
-        </ol>
-      </div>
-      <div className="w-full">
-        <H3 className="">Modes of B9 Game</H3>
-        <P>B9 offers a variety of bingo modes to make the game exciting afresh.  The classic mode is standard bingo in which you can win by marking a full row and columns.  In blackout mode, the challenge increases and you have to cover numbers on your bingo card to win.  There is timed mode also in which you race against the clock to complete patterns before time finished. These modes give players different ways to enjoy the game.</P>
-      </div>
-      <div className="w-full">
-        <H3 className="">Games varieties in B9</H3>
-        <ol className="">
-          <li>Teen Patti</li>
-          <li>Ludo</li>
-          <li>Joker</li>
-          <li>Andar Bahar</li>
-          <li>Mines</li>
-          <li>Pirate Flip</li>
-          <li>aviator</li>
-        </ol>
+    <React.Fragment>
+      <header className="flex items-center bg-black justify-between text-white">
+        <Image src="/header-logo.jpg" alt="Picture of the author" width={200} height={70} />
+      </header>
+      <div className="flex flex-col items-center gap-8 max-w-screen-sm text-center mx-auto my-16">
+        <H2>B9 Game Download (Earning APP Pakistan) For Android 2024</H2>
+        <Link href="https://m.b9game3.com/refer/MDM0NTM0OTQ3NzM=">
+          <Button size={'lg'} className='min-w-60 bg-green-800 text-lg font-bold'>
+            Download
+          </Button>
+        </Link>
+        <div>
+          <Table className="w-72 text-left border-none">
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                  Size
+                </TableCell>
+                <TableCell>
+                  23.78 MB
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  Version
+                </TableCell>
+                <TableCell>
+                  v1.0.22
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  Requirements
+                </TableCell>
+                <TableCell>
+                  Android 6+
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  Category
+                </TableCell>
+                <TableCell>
+                  Card
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  Download
+                </TableCell>
+                <TableCell>
+                  Free of cost
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+          
+        </div>
+        <Image src="/banner-image-hd.png" alt="Picture of the author" className="max-w-screen-lg  w-screen" width={500} height={500} />
+        <P>The B9 game is the Latest earing Application that you can play on your phone, computer, or tablet. In this game, you get a bingo card with numbers on it, and the numbers are called out, and you can mark them off. In B9 players get a virtual bingo card filled with numbers arranged in a grid. When numbers are drawn randomly players mark off corresponding numbers on their cards. B9 gaming is free of cost for Android and the account opening bonus is 15 rupees. The main protection is a referral bonus for players. Everyone can use their account ID as an invitation code. It provides unlimited earnings for players in Pakistan.</P>
+        <P>B9 includes features like power-up players and a customizable theme and also makes it an engaging experience for players of all ages. By playing the B9 game you can earn real money while playing your favorite game. This game is made to challenge your brain while keeping you entertained for hours. A small indie team developed the B9 game. It was first released in 2024 and became unique because of its simplicity and strategy.  It is available for only Android platforms. You can download it from the Google Play or app store.</P>
+        <div className="w-full">
+          <H3 className="">B9 Bonus</H3>
+          <P>it&apos;s a cooperative game to earn a bonus in rupees, for the game bonus play more and more. There is no limit to playing and bonus collection. In many ways, you can earn a bonus by using tricks and tips.</P>
+        </div>
+        <div className="w-full">
+          <H3 className="">B9 Tips And Tricks</H3>
+          <P>trips and tricks are used by players to get more cash on gameplay. Use IQ to detect game playouts and easily win. In the other hand you can use your game ID account to get more cash on friends&apos; inventions and </P>
+        </div>
+        <div className="w-full">
+          <H3 className="">B9 Game Features</H3>
+          <ol className="">
+            <li>
+              <H5>3x3 Grid</H5>
+              <P>A small bingo card with 9 numbers in a 3x3 layout.</P>
+            </li>
+            <li>
+              <H5>Fast Gameplay</H5>
+              <P>Fastly round due to reduced number of numbers.</P>
+            </li>
+            <li>
+              <H5>Winning patterns</H5>
+              <P>Players can win by making specific patterns.</P>
+            </li>
+            <li>
+              <H5>Online features</H5>
+              <P>Includes many chats, options, and customization.</P>
+            </li>
+            <li>
+              <H5>Auto Daubing</H5>
+              <P>There are options to automatically mark called numbers.</P>
+            </li>
+            <li>
+              <H5>Number pool</H5>
+              <P>Few numbers are called compared to traditional bingo.</P>
+            </li>
+          </ol>
+        </div>
+        <div className="w-full">
+          <H3 className="">Modes of B9 Game</H3>
+          <P>B9 offers a variety of bingo modes to make the game exciting afresh.  The classic mode is standard bingo in which you can win by marking a full row and columns.  In blackout mode, the challenge increases and you have to cover numbers on your bingo card to win.  There is timed mode also in which you race against the clock to complete patterns before time finished. These modes give players different ways to enjoy the game.</P>
+        </div>
+        <div className="w-full">
+          <H3 className="">Games varieties in B9</H3>
+          <ol className="">
+            <li>Teen Patti</li>
+            <li>Ludo</li>
+            <li>Joker</li>
+            <li>Andar Bahar</li>
+            <li>Mines</li>
+            <li>Pirate Flip</li>
+            <li>aviator</li>
+          </ol>
 
+        </div>
+        <div className="w-full" >
+          <H3 className="">B9 Game Review</H3>
+          <P>It is a basic and easy game to earn money. It has a simple format making it easy and great for all ages.  The game has different bingo modes and colorful themes which make it good to play.  It runs smoothly and loads quickly. B9 has no extra features or options. It is perfect for those people who want a quick and simple bingo game.  B9 is a good choice for those who like straightforward bingo experience.</P>
+        </div>
+        <div className="w-full">
+          <H3 className="">Conclusion</H3>
+          <P>The B9 game is just a card game. It is an appearance that has seized the hearts of gamers worldwide.  It has many unique blends of engaging storylines, strategy, and strong community aspects. If you are a good gamer or not but B9 game offers something for everyone. The major features. You can earn money by taking part in various games like sports betting, live casino, and more.  In 2024 it is the best platform for earning money with great excitement.  Its features make a truly captivating experience for users.</P>
+        </div>    
       </div>
-      <div className="w-full" >
-        <H3 className="">B9 Game Review</H3>
-        <P>It is a basic and easy game to earn money. It has a simple format making it easy and great for all ages.  The game has different bingo modes and colorful themes which make it good to play.  It runs smoothly and loads quickly. B9 has no extra features or options. It is perfect for those people who want a quick and simple bingo game.  B9 is a good choice for those who like straightforward bingo experience.</P>
-      </div>
-      <div className="w-full">
-        <H3 className="">Conclusion</H3>
-        <P>The B9 game is just a card game. It is an appearance that has seized the hearts of gamers worldwide.  It has many unique blends of engaging storylines, strategy, and strong community aspects. If you are a good gamer or not but B9 game offers something for everyone. The major features. You can earn money by taking part in various games like sports betting, live casino, and more.  In 2024 it is the best platform for earning money with great excitement.  Its features make a truly captivating experience for users.</P>
-      </div>    
-    </div>
+    <footer className="flex items-center bg-black justify-between text-white px-8 py-2">
+      <H3>B9 Game Download</H3>
+      <P>© 2024 B9 Game Download</P>  
+    </footer>
+    </React.Fragment>
   );
 }
